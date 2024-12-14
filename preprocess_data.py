30360b4f413a82e75529431988d881bcbb4dbd96
Mihnea-Alexandru VÎRLAN (116034) <mihnea.virlan@fep8.grid.pub.ro>
2024-12-14 15:24:10 +0200

    Added text cleanup and reorganized the preprocessing step

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from typing import Tuple, List, Dict
 
@@ -9,8 +10,25 @@ from sklearn.model_selection import train_test_split
 
 from torch.utils.data import Dataset, DataLoader
 
-from langdetect import detect
+from fast_langdetect import detect
 from emoji import demojize
+import re
+from tqdm import tqdm
+
+
+def text_cleanup(text:str):
+    text = demojize(text)
+    text = re.sub(r"<[A-Za-z0-9 /;:\"]*>",r" ",text)
+    text = re.sub(r"([a-zA-Z0-9])\(",r"\1 (",text)
+    text = re.sub(r"[*#@]","",text)
+    text = re.sub(r"(\. ){2,}",r". ",text)
+    text = re.sub(r"(\- ){2,}",r"",text)
+    text = re.sub(r"[^a-zA-Z0-9.,;:\"' ()\-,!?/]",r" ",text)
+    text = re.sub(r"([a-zA-Z0-9]) {1,}([.,;:\"\-])",r"\1\2",text)
+    text = re.sub(r"([a-zA-Z0-9])([\"])([a-zA-Z0-9])",r"\1 \2\3",text)
+    text = re.sub(r"([a-zA-Z])- ",r"\1 - ",text)
+    text = re.sub(r" {2,}",r" ",text)
+    return text
 
 class SentimentAnalysisDataset(Dataset):
     def __init__(self, df: pd.DataFrame):
@@ -39,17 +57,11 @@ def get_data_location():
         return '/kaggle/input'
     else:
         return './datasets'
-    
-def detect_language(text: str) -> str:
-    try:
-        return detect(text)
-    except Exception:
-        return 'unknown'
 
-def load_sentiment_analysis_dataset() -> Tuple[DataLoader, DataLoader]:
+def load_sentiment_analysis_dataset() -> Tuple[pd.DataFrame,pd.DataFrame]:
     DATA_LOCATION = get_data_location() + '/sentiment_analysis_dataset/'
     train_df = pd.DataFrame(columns = ['Content', 'Label'])
-    for file in os.listdir(DATA_LOCATION + 'train/pos'):
+    for file in tqdm(os.listdir(DATA_LOCATION + 'train/pos'),desc="Train_pos_load"):
         with open(DATA_LOCATION + 'train/pos/' + file, 'r') as f:
             label = file.split('_')[1].split('.')[0]
             content = f.read()
@@ -57,10 +69,10 @@ def load_sentiment_analysis_dataset() -> Tuple[DataLoader, DataLoader]:
             index = content.find('Rating') # filter out the rating from the content
             if index != -1:
                 content = content[:index]
-            
-            train_df = pd.concat([train_df, pd.DataFrame([{'Content': content, 'Label': label}])], ignore_index = True)
+            if(detect(content)["lang"]=="en"):
+                train_df = pd.concat([train_df, pd.DataFrame([{'Content': content, 'Label': int(label)-1}])], ignore_index = True)
 
-    for file in os.listdir(DATA_LOCATION + 'train/neg'):
+    for file in tqdm(os.listdir(DATA_LOCATION + 'train/neg'),desc="Train_neg_load"):
         with open(DATA_LOCATION + 'train/neg/' + file, 'r') as f:
             label = file.split('_')[1].split('.')[0]
             content = f.read()
@@ -68,13 +80,13 @@ def load_sentiment_analysis_dataset() -> Tuple[DataLoader, DataLoader]:
             index = content.find('Rating') # filter out the rating from the content
             if index != -1:
                 content = content[:index]
+            if(detect(content)["lang"]=="en"):
+                train_df = pd.concat([train_df, pd.DataFrame([{'Content': content, 'Label': int(label)-1}])], ignore_index = True)
 
-            train_df = pd.concat([train_df, pd.DataFrame([{'Content': content, 'Label': label}])], ignore_index = True)
-
-    train_df['Content'] = train_df['Content'].apply(demojize)
+    train_df['Content'] = train_df['Content'].apply(text_cleanup)
 
     test_df = pd.DataFrame(columns = ['Content', 'Label'])
-    for file in os.listdir(DATA_LOCATION + 'test/pos'):
+    for file in tqdm(os.listdir(DATA_LOCATION + 'test/pos'),desc="Test_pos_load"):
         with open(DATA_LOCATION + 'test/pos/' + file, 'r') as f:
             label = file.split('_')[1].split('.')[0]
             content = f.read()
@@ -82,10 +94,10 @@ def load_sentiment_analysis_dataset() -> Tuple[DataLoader, DataLoader]:
             index = content.find('Rating') # filter out the rating from the content
             if index != -1:
                 content = content[:index]
-            
-            train_df = pd.concat([train_df, pd.DataFrame([{'Content': content, 'Label': label}])], ignore_index = True)
+            if(detect(content)["lang"]=="en"):
+                test_df = pd.concat([test_df, pd.DataFrame([{'Content': content, 'Label': int(label)-1}])], ignore_index = True)
 
-    for file in os.listdir(DATA_LOCATION + 'test/neg'):
+    for file in tqdm(os.listdir(DATA_LOCATION + 'test/neg'),desc="Test_neg_load"):
         with open(DATA_LOCATION + 'test/neg/' + file, 'r') as f:
             label = file.split('_')[1].split('.')[0]
             content = f.read()
@@ -93,22 +105,20 @@ def load_sentiment_analysis_dataset() -> Tuple[DataLoader, DataLoader]:
             index = content.find('Rating') # filter out the rating from the content
             if index != -1:
                 content = content[:index]
-            
-            train_df = pd.concat([train_df, pd.DataFrame([{'Content': content, 'Label': label}])], ignore_index = True)
+            if(detect(content)["lang"]=="en"):
+                test_df = pd.concat([test_df, pd.DataFrame([{'Content': content, 'Label': int(label)-1}])], ignore_index = True)
 
-    test_df['Content'] = test_df['Content'].apply(demojize)
+    test_df['Content'] = test_df['Content'].apply(text_cleanup)
     
-    train_dataloader = DataLoader(SentimentAnalysisDataset(train_df), batch_size = 32, shuffle = True)
-    test_dataloader = DataLoader(SentimentAnalysisDataset(test_df), batch_size = 32, shuffle = False)
-    return train_dataloader, test_dataloader
+    return train_df,test_df
 
-def load_hate_speech_dataset() -> Tuple[DataLoader, DataLoader]:
+def load_hate_speech_dataset() -> Tuple[pd.DataFrame, pd.DataFrame]:
     FILE = get_data_location() + '/hate_speech_dataset/HateSpeechDataset.csv'
     df = pd.read_csv(FILE)
     df = df.dropna().drop_duplicates()
-    df['Content'] = df['Content'].apply(demojize) # convert emojis to text
+    df['Content'] = df['Content'].apply(text_cleanup) # convert emojis to text
     df = shuffle(df, random_state = 42) # randomly shuffle the dataset
     train_df, test_df = train_test_split(df, test_size = 0.2, random_state = 42) # split the dataset into training and testing sets
-    train_dataloader = DataLoader(HateSpeechDataset(train_df), batch_size = 128, shuffle = True)
-    test_dataloader = DataLoader(HateSpeechDataset(test_df), batch_size = 128, shuffle = False)
-    return train_dataloader, test_dataloader
+    #train_dataloader = DataLoader(HateSpeechDataset(train_df), batch_size = 128, shuffle = True)
+    #test_dataloader = DataLoader(HateSpeechDataset(test_df), batch_size = 128, shuffle = False)
+    return train_df,test_df
