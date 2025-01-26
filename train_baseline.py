9026875bdfbd5c40003017f590430c2f850f3818
Mihnea-Alexandru VÎRLAN (116034) <mihnea.virlan@fep8.grid.pub.ro>
2025-01-26 04:06:43 +0200

    Added script flexibility

--- train_baseline.py
+++ train_baseline.py
@@ -1,62 +1,71 @@
 from torch.utils.data import Dataset,DataLoader
 import pandas as pd
 from preprocess_data import load_sentiment_analysis_dataset,load_hate_speech_dataset
-import random
+import argparse
 from transformers import BertTokenizer, BertModel, DistilBertModel
 import lightning as pl
 import torch.nn as nn
 import torch
 from aim.pytorch_lightning import AimLogger
-from torchmetrics.classification import MulticlassAccuracy,MulticlassF1Score
+from torchmetrics.classification import MulticlassAccuracy,MulticlassF1Score,BinaryF1Score,BinaryAccuracy
 import argparse
 import transformers
-import math
-import re
+
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-acc_sent = MulticlassAccuracy(num_classes=2).to(device)
-acc_hate = MulticlassAccuracy(num_classes=3).to(device)
-f1_sent = MulticlassF1Score(num_classes=2).to(device)
-f1_hate = MulticlassF1Score(num_classes=3).to(device)
+
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-
+acc_sent = BinaryAccuracy(ignore_index=tokenizer.pad_token_id).to(device)
+acc_hate = MulticlassAccuracy(num_classes=3,ignore_index=tokenizer.pad_token_id).to(device)
+f1_sent = BinaryF1Score(ignore_index=tokenizer.pad_token_id).to(device)
+f1_hate = MulticlassF1Score(num_classes=3,ignore_index=tokenizer.pad_token_id).to(device)
 map_labels_type_data={
     2:"sent",
     3:"hate"
 }
-num_epochs=20
-batch_size = 16
+
+class BERTSeqClassif(nn.Module):
+    def __init__(self,num_labels,freeze_weights=False):
+        super().__init__()
+        self.baseline_bert = BertModel.from_pretrained("bert-base-uncased",torch_dtype=torch.bfloat16)
+        if(freeze_weights==True):
+            for param in self.baseline_bert.parameters():
+                param.requires_grad=False
+        self.dropout = nn.Dropout(p=0.2)
+        self.classifier_head = nn.Linear(self.baseline_bert.config.hidden_size,num_labels,dtype=torch.bfloat16)
+    def forward(self,batch):
+        content = batch[0]
+        tokenized_content = tokenizer(content,return_tensors="pt",truncation=True,padding='longest',max_length=128)
+        out_bert = self.baseline_bert(input_ids=tokenized_content["input_ids"].to(device),attention_mask=tokenized_content["attention_mask"].to(device))
+        y_hat = self.classifier_head(self.dropout(out_bert.pooler_output))
+        return y_hat
+
 class BaselineModule(pl.LightningModule):
-    def __init__(self,num_labels,total_steps):
+    def __init__(self,num_labels,total_steps,lr,batch_size,warmup_steps,freeze_weights=False):
         super().__init__()
         self.save_hyperparameters()
-        self.baseline_bert = BertModel.from_pretrained("bert-base-uncased",torch_dtype=torch.bfloat16)
-        self.baseline_bert.train()
-        """for name,param in self.baseline_bert.named_parameters():
-            digit_in_name = re.findall("[0-9]+",name)
-            if(len(digit_in_name)==1):
-                if(int(digit_in_name[0])<6):
-                    param.requires_grad=False
-            print(name,param.requires_grad)"""
-        self.dropout = nn.Dropout(p=0.15)
-        self.classifier_head = nn.Linear(self.baseline_bert.config.hidden_size,num_labels,dtype=torch.bfloat16)
-        self.loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
+        self.BERT_seq_classif = BERTSeqClassif(num_labels,freeze_weights)
         self.acc_fn = acc_sent if num_labels==2 else acc_hate
         self.f1_fn = f1_sent if num_labels==2 else f1_hate
         self.num_labels=num_labels
         self.total_steps = total_steps
-        self.warmup_steps=4000
+        self.warmup_steps=warmup_steps
+        self.sch_type =sch_type
+        self.lr=lr
+        self.batch_size=batch_size
         self.automatic_optimization = False
+        self.loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id)
+
     def compute_metrics(self,y_hat,labels,mode):
-        self.log(f"{mode}_accuracy_{map_labels_type_data[self.num_labels]}",self.acc_fn(y_hat,labels),batch_size=batch_size,on_step=False,on_epoch=True)
-        self.log(f"{mode}_f1_{map_labels_type_data[self.num_labels]}",self.f1_fn(y_hat,labels),batch_size=batch_size,on_step=False,on_epoch=True)
+        preds_probs = torch.softmax(y_hat,dim=1)
+        preds = torch.argmax(preds_probs,dim=1)
+        self.log(f"{mode}_accuracy_{map_labels_type_data[self.num_labels]}",self.acc_fn(preds,labels),batch_size=self.batch_size,on_step=False,on_epoch=True)
+        self.log(f"{mode}_f1_{map_labels_type_data[self.num_labels]}",self.f1_fn(preds,labels),batch_size=self.batch_size,on_step=False,on_epoch=True)
     def compute_loss(self,batch,mode):
-        content = batch[0]
-        tokenized_content = tokenizer(content,return_tensors="pt",truncation=True,padding='max_length',max_length=128)
         labels = torch.tensor(batch[1]).to(device)
-        out_bert = self.baseline_bert(input_ids=tokenized_content["input_ids"].to(device),attention_mask=tokenized_content["attention_mask"].to(device))
-        y_hat = self.classifier_head(self.dropout(out_bert.pooler_output))
+        y_hat = self.BERT_seq_classif(batch)
         self.compute_metrics(y_hat,labels,mode)
         return self.loss_fn(y_hat,labels)
     def training_step(self,batch,batch_idx):
@@ -68,37 +77,58 @@ class BaselineModule(pl.LightningModule):
         opt.step()
         #if((batch_idx + 1) % 500 == 0):
         sch.step()
-        self.log("Train_loss",loss_total,batch_size=batch_size,on_step=False,on_epoch=True)
+        self.log("Train_loss",loss_total,batch_size=self.batch_size,on_step=False,on_epoch=True)
         return loss_total
     def validation_step(self,batch,batch_idx):
         loss_total = self.compute_loss(batch,"Test")
-        self.log("Test_loss",loss_total,batch_size=batch_size,on_step=False,on_epoch=True)
+        self.log("Test_loss",loss_total,batch_size=self.batch_size,on_step=False,on_epoch=True)
     def configure_optimizers(self):
         #optimizer = torch.optim.SGD(self.parameters(),lr=1e-3,momentum=0.9,weight_decay=0.01)
-        optimizer = torch.optim.AdamW(self.parameters(),lr=5e-5)
+        optimizer = torch.optim.Adam(self.parameters(),lr=self.lr)
         #scheduler = transformers.get_linear_schedule_with_warmup(optimizer,500,self.dataloader_len*num_epochs)
-        scheduler = transformers.get_scheduler("linear",optimizer,0,self.total_steps)
+        scheduler = transformers.get_scheduler(self.sch_type,optimizer,self.warmup_steps,self.total_steps)
         #return optimizer
         return [optimizer],[scheduler]
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset",choices=["hate","sent"])
-    parsed_arg = parser.parse_args()
-    dataset_type=parsed_arg.dataset
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--num_epochs")
+    arg_parser.add_argument("--batch_size")
+    arg_parser.add_argument("--sch_type",choices=["linear","cosine","inverse_sqrt"],default="linear")
+    arg_parser.add_argument("--lr")
+    arg_parser.add_argument("--dataset",choices=["hate","sent"])
+    arg_parser.add_argument("--warmup_steps")
+    parsed_args = arg_parser.parse_args()
+    num_epochs = int(parsed_args.num_epochs)
+    batch_size = int(parsed_args.batch_size)
+    sch_type = parsed_args.sch_type
+    lr = float(parsed_args.lr)
+    dataset_type = parsed_args.dataset
+    warmup_steps = int(parsed_args.warmup_steps)
     seed_value=42
     pl.seed_everything(seed_value)
     if(dataset_type=="hate"):
-        trainLoader,testLoader = load_hate_speech_dataset()
+        train_df,test_df = load_hate_speech_dataset(batch_size=batch_size)
+        trainLoader = DataLoader(train_df,batch_size=batch_size,shuffle=True)
+        testLoader = DataLoader(test_df,batch_size=batch_size,shuffle=False)
         num_labels = 3
-        #batch_example = next(iter(trainLoader))
-        #print(torch.tensor(batch_example[1]))
+        freeze_weights=False
     else:
-        trainLoader,testLoader =load_sentiment_analysis_dataset()
+        train_df,test_df =load_sentiment_analysis_dataset(batch_size=batch_size)
+        trainLoader = DataLoader(train_df,batch_size=batch_size,shuffle=True)
+        testLoader = DataLoader(test_df,batch_size=batch_size,shuffle=False)
         num_labels = 2
+        freeze_weights = True
 
     MTLlog = AimLogger(experiment=f"Baseline_BERT_{dataset_type}",train_metric_prefix="Train_",val_metric_prefix="Test_")
+    MTLlog.log_hyperparams({
+        "sch_type":sch_type,
+        "batch_size":batch_size,
+        "lr":lr,
+        "num_epochs":num_epochs
+    })
+    csv_logger = pl.pytorch.loggers.csv_logs.CSVLogger(f"Baseline_{dataset_type}")
     checkpoint_callback = pl.pytorch.callbacks.ModelCheckpoint(dirpath=f"Baseline_Model_{dataset_type}_CKPT")
-    pl_model = BaselineModule(num_labels,len(trainLoader)* num_epochs)
-    trainer = pl.Trainer(max_epochs=num_epochs,logger=MTLlog,callbacks=[checkpoint_callback],precision="bf16")
+    pl_model = BaselineModule(num_labels,len(trainLoader)* num_epochs,lr,batch_size,warmup_steps,freeze_weights=freeze_weights)
+    trainer = pl.Trainer(max_epochs=num_epochs,logger=[csv_logger,MTLlog],callbacks=[checkpoint_callback],precision="bf16")
     trainer.fit(pl_model,trainLoader,testLoader)
